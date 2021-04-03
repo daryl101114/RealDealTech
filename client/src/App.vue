8f640ca6eb89dc2a92190095331b37bf2d9e8faf
Justin <jpsulak@gmail.com>
2021-04-02 21:33:06 -0500

    Testing commented lines

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
+      <!-- Router View Pushed -->
       <router-link to="https://www.google.com">Google</router-link>
     </div>
     <router-view/>
