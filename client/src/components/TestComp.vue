520fb8613ed71114ca9e644fe5ad02d8d2123bea
Justin <jpsulak@gmail.com>
2021-04-02 22:36:53 -0500

    Adding a new component to the project for testing

---
+++ client/src/components/TestComp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="testComp">
+    <h1>{{ msg }}</h1>
+    <h2>This is a page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+h2 {
+  color: #b94279;
+}
+</style>
--- client/src/views/Test.vue
+++ client/src/views/Test.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="test">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="This is the Test Page"/>
+    <TestComp msg="This is the Test Page"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TestComp from '@/components/TestComp.vue'
 
 export default {
   name: 'Test',
   components: {
-    HelloWorld
+    TestComp
   }
 }
 </script>
