37448f53fedb660e883f817c78aa350d057ff0a4
Justin <jpsulak@gmail.com>
2021-04-08 22:16:12 -0500

    changing stuff around in the app vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link> |
       <router-link to="/clients">Clients</router-link> |
       <router-link to="/dogs">Dogs</router-link> |
-      <router-link to="/stays">Stays</router-link>
+      <router-link to="/stays">Stays</router-link> |
+      <router-link to="/">Home</router-link> |
+      <router-link to="/test">Test</router-link>
+      <!-- <router-link to="/about">About</router-link> | -->
     </div>
     <router-view/>
   </div>
