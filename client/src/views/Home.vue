afa6d02e06319770578e0779711d794d4acc13ef
Justin <jpsulak@gmail.com>
2021-04-08 20:58:11 -0500

    Changed name of HelloWorld to HelloStart in order to try to merge branch with FrontEnd

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloStart msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloStart from '@/components/HelloStart.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloStart
   }
 }
 </script>
