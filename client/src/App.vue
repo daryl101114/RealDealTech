ddafc992899935cf104e5794ff9e6e7b6206ac5e
Justin <jpsulak@gmail.com>
2021-04-08 22:09:26 -0500

    Added spacing in the navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link>
+      <router-link to="/test">Test</router-link> |
       <router-link to="/clients">Clients</router-link> |
       <router-link to="/dogs">Dogs</router-link> |
       <router-link to="/stays">Stays</router-link>
