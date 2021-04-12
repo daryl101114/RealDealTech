26527bbd45db544bc061a44567130e3dbf0d3c76
Justin <jpsulak@gmail.com>
2021-04-11 21:58:02 -0500

    added extra buttons in the footer

--- client/src/views/AddDog.vue
+++ client/src/views/AddDog.vue
@@ -75,6 +75,20 @@
                 <router-link to="/dogs" class="btn btn-success form-button p-2">Cancel</router-link>
             </div>
         </form>
+
+        <footer class="page-footer font-small blue fixed-bottom">
+            <div class="d-flex flex-row-reverse bd-highlight foot">
+                <!-- button to cancel -->
+                <router-link to="/dogs">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#5cb85c" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <!-- <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/> -->
+                        <!-- <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/> -->
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                    </svg>
+                </router-link>
+            </div>
+        </footer>
     </div>
 </template>
 
