ef4f188278980b2ee0791d748e133bc94ecde37a
Justin <jpsulak@gmail.com>
2021-04-13 18:39:31 -0500

    began styling navbar. tested w mobile screens

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/clients">Clients</router-link> |
-      <router-link to="/dogs">Dogs</router-link> |
-      <router-link to="/stays">Stays</router-link> |
-      <router-link to="/">Home</router-link> |
+      <router-link to="/clients">Clients</router-link> 
+      <router-link to="/dogs">Dogs</router-link> 
+      <router-link to="/stays">Stays</router-link> 
+      
+      <!-- <router-link to="/">Home</router-link> -->
+      <!-- | -->
       <router-link to="/test">Test</router-link>
       <!-- <router-link to="/about">About</router-link> | -->
     </div>
@@ -28,16 +30,48 @@
 }
 
 #nav {
+  overflow: hidden;
   padding: 30px;
   color: aliceblue;
+  background:#2c3e50;
+  /* background: darkgreen; */
+  padding: 1rem;
+  margin-bottom: 1.5rem;
 }
 
+/* .sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+.sticky + .content {
+  padding-top: 60px;
+} */
+
 #nav a {
+  
+  text-decoration: none;
+  padding: 1rem;
   font-weight: bold;
+  /* background:#2b3850; */
   color: aliceblue;
+  text-align: center;
+}
+
+#nav a:hover{
+  background-color:#132e49;
+  color: whitesmoke;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+  background-color:#1f3041;
+}
+
+@media only screen and (max-width: 600px) {
+    #nav a {
+        display: flex;
+        flex-direction: column;
+    }
 }
 </style>
