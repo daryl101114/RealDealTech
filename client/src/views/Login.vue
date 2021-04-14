6b2e0ca53b8faadbc86dd5a526533dc763fce32f
Justin <inogames@gmail.com>
2021-04-14 00:32:01 -0500

    Rewrote Login Page and some formatting

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,23 +1,56 @@
 <template>
   <div class="login">
     <img id="logo" alt="Vue logo" src="../assets/ourLogo.png">
-    <h1 class="page-heading text-light">Log In</h1>
+    <h1 class="page-heading text-light">Login</h1>
 
-    <!-- Username -->
-        <div class="d-flex justify-content-center">
-            <input type="text" class="form-control input-field" id="search-bar" placeholder="Username">
-        </div>
+    <form v-on:submit="login">
+      <!-- Username -->
+      <div class="d-flex justify-content-center">
+          <input type="text" class="form-control input-field" id="search-bar" placeholder="Enter Username" v-model="username">
+      </div>
 
-    <!-- Password -->
-        <div class="d-flex justify-content-center">
-            <input type="text" class="form-control input-field" id="search-bar" placeholder="Password">
-        </div>    
+      <!-- Password -->
+      <div class="d-flex justify-content-center">
+          <input type="password" class="form-control input-field" id="search-bar" placeholder="Enter Password" v-model="password">
+      </div>
+
+      <!-- Login Button -->
+      <input type="submit" class="btn btn-success" value="Login">
+    </form>
+</div>
+ 
+</template>
+
   </div>
 </template>
+
 <script>
+import axios from 'axios'
 
 export default {
- 
+  name: 'Login',
+  data() {
+    return {
+      username: null,
+      password: null,
+    }
+  },
+  methods: {
+    login(e) {
+      e.preventDefault();
+
+      axios.post("http://localhost:3000/api/users/login", {
+        username: this.username,
+        password: this.password,
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
--- server/api/controller/user.js
+++ server/api/controller/user.js
@@ -2,7 +2,7 @@
 const db = require("../../models/db.connect")
 const User = db.users
 const Op = db.Sequelize.Op
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const passport = require('passport')
 
 const initializePassport = require('../../config/passport-config')
@@ -13,28 +13,28 @@ exports.create = (req, res) => {
   if (!req.body.username) {
     res.status(400).send({
       message: "Content can not be empty!"
-    });
-    return;
+    })
+    return
   }
   bcrypt.hash(req.body.password, 10).then((hash)=>{
     const user = {
       username: req.body.username,
       password: hash,
-    };
+    }
     User.create(user).then(data => {
-      res.send(data);
+      res.send(data)
     }).catch(err => {
       res.status(500).send({message: err.message || "Some error occured"})
     })
   })
-};
+}
 //--------------------------------------------------------------------------------
 
 //LOGIN
-exports.signin = (req, res, next) =>{
+exports.login = (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/api/user/signup',
+    failureRedirect: '/api/users/login',
     failureFlash: true
   })(req, res, next)
 }
