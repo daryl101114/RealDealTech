ee01474fa2ff5fcd30bc92a232a4767caa491bf5
Damian Falcon <dfalcon005@gmail.com>
2021-04-15 15:55:59 -0500

    Cleaned up update pages and routed back to details after updating

--- client/src/views/UpdateClient.vue
+++ client/src/views/UpdateClient.vue
@@ -56,15 +56,13 @@
             <div class="d-flex flex-row">
                 <!-- May need input type="submit" instead? -->
                 <button type="submit" class="btn btn-success form-button p-2">Submit</button>
-                <input type="reset" value="Clear" class="btn btn-success form-button p-2">                
-                <!-- <router-link to="/clients" class="btn btn-success form-button p-2">Cancel</router-link> -->
             </div>
         </form>
 
         <footer class="page-footer font-small blue fixed-bottom">
             <div class="d-flex flex-row-reverse bd-highlight foot">
                 <!-- button to cancel -->
-                <router-link to="/clients">
+                <router-link v-bind:to="'/clientdetails/' + this.id">
                     <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#5cb85c" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                     </svg>
--- client/src/views/UpdateDog.vue
+++ client/src/views/UpdateDog.vue
@@ -87,15 +87,13 @@
             <!-- submit and cancel buttons -->
             <div class="d-flex flex-row">
                 <button type="submit" class="btn btn-success form-button p-2">Submit</button>
-                <input type="reset" value="Clear" class="btn btn-success form-button p-2">
-                <!-- <router-link to="/dogs" class="btn btn-success form-button p-2">Cancel</router-link> -->
             </div>
         </form>
 
         <footer class="page-footer font-small blue fixed-bottom">
             <div class="d-flex flex-row-reverse bd-highlight foot">
                 <!-- button to cancel -->
-                <router-link to="/dogs">
+                <router-link v-bind:to="'/dogdetails/' + this.id">
                     <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#5cb85c" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                         <!-- <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/> -->
