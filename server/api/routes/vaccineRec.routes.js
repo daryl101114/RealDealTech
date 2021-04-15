7d1a3a333f4730c088f9d6115e1e0e7da892cd79
daryl101114 <daryl101114@gmail.com>
2021-04-15 00:19:45 -0500

    Vaccine Record API

--- server/api/routes/vaccineRec.routes.js
+++ server/api/routes/vaccineRec.routes.js
@@ -1,8 +1,22 @@
 module.exports = app => {
     var router = require('express').Router()
     const vaccRec = require("../controller/vaccineRec.controller")
-
-    router.get('/', vaccRec.getAll)
+    const multer = require('multer')
+    //STORAGE STRATEGY
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb( null, './uploads/')
+        },
+        filename: function(req, file, cb){
+            const ext = file.mimetype.split("/")[1];
+            cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+        }
+    })
+    const upload = multer({storage: storage})
+    //-----------------------------------------------------------
+    
+    router.get('/record/:id', vaccRec.getOne);
+    router.post('/post', upload.single('vaccine_file') ,vaccRec.create);
 
     app.use("/api/vaccine", router)
 }
